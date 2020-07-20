3dbd0eee84c58abe6a6ccaaf253ae68781ba95ec
mshanemc <shane.mclaughlin@salesforce.com>
2020-07-20 12:53:36 -0500

    reset component to initial state

--- force-app/main/default/lwc/demoComponent/demoComponent.js
+++ force-app/main/default/lwc/demoComponent/demoComponent.js
@@ -1,16 +1,3 @@
 import { LightningElement, wire } from 'lwc';
-import Demo from '@salesforce/apex/Demo.Demo';
 
-export default class DemoComponent extends LightningElement {
-  @wire(Demo, {})
-  Accounts;
-
-  constructor() {
-    super();
-    console.log('hello from the constructor');
-  }
-
-  connectedCallback() {
-    console.log('hello from the connectedCallback');
-  }
-}
+export default class DemoComponent extends LightningElement {}
